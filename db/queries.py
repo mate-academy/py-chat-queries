ae1d10a0bce859b446ac057251355b7fd6988fda
Oleg Fadeev <drivemax26@gmail.com>
2022-07-09 17:52:02 +0300

    Solution

--- db/queries.py
+++ db/queries.py
@@ -17,7 +17,7 @@ def get_users_who_sent_messages_in_2015() -> list[str]:
 
 
 def get_actual_chats() -> list[Chat]:
-    return list(Chat.objects.filter(message__sent__year__gte=2020))
+    return list(Chat.objects.filter(message__sent__year__gt=2020))
 
 
 def get_messages_contain_authors_first_name():
@@ -36,7 +36,8 @@ def get_delivered_or_admin_messages() -> list[Message]:
 
 def get_count_messages_sent_by_first_name(first_name: str) -> int:
     return Message.objects.filter(
-        user__first_name=first_name).aggregate(Count('id'))
+        user__first_name=first_name
+    ).count()
 
 
 def get_top_users_by_number_of_the_messages() -> list[User]:
