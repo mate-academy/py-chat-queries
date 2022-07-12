00c73fa93dd3df560c491549834d1d16384214a5
Anton Lasko <anton.lasko1993@gmail.com>
2022-07-12 09:04:04 +0300

    'Solution'

--- db/queries.py
+++ db/queries.py
@@ -3,54 +3,45 @@ from django.db.models import Q, Count
 
 
 def get_messages_that_contain_word(word: str) -> list[Message]:
-    queryset = Message.objects.filter(text__icontains=word)
-    return [message for message in queryset]
+    return list(Message.objects.filter(text__icontains=word))
 
 
 def get_untitled_chats() -> list[Chat]:
-    queryset = Chat.objects.filter(title__startswith="Untitled")
-    return [chat for chat in queryset]
+    return list(Chat.objects.filter(title__startswith="Untitled"))
 
 
 def get_users_who_sent_messages_in_2015() -> list[str]:
-    queryset = User.objects.filter(
-        message__sent__year=2015).values_list("first_name", "last_name")
-    return [name for name in queryset]
+    return list(User.objects.filter(
+        message__sent__year=2015).values_list("first_name", "last_name"))
 
 
 def get_actual_chats() -> list[Chat]:
-    queryset = Chat.objects.filter(message__sent__year__gte=2020).distinct()
-    return [chat for chat in queryset]
+    return list(Chat.objects.filter(message__sent__year__gte=2020).distinct())
 
 
 def get_messages_contain_authors_first_name():
     names = User.objects.all().values_list("first_name")
-    queryset = Message.objects.filter(text__icontains=names)
-    return [text for text in queryset]
+    return list(Message.objects.filter(text__icontains=names))
 
 
 def get_users_who_sent_messages_starts_with_m_or_a() -> list[User]:
-    queryset = User.objects.filter(Q
-                                   (message__text__startswith="a") | Q
-                                   (message__text__startswith="m")).distinct()
-    return [user for user in queryset]
+    return list(User.objects.filter(Q
+                (message__text__startswith="a") | Q
+                (message__text__startswith="m")).distinct())
 
 
 def get_delivered_or_admin_messages() -> list[Message]:
-    queryset = Message.objects.filter(
-        Q(user__username__startswith="admin") | Q(is_delivered=1))
-    return [message for message in queryset]
+    return list(Message.objects.filter(
+        Q(user__username__startswith="admin") | Q(is_delivered=1)))
 
 
 def get_count_messages_sent_by_first_name(first_name: str) -> int:
-    queryset = Message.objects.filter(user__first_name=first_name).count()
-    return queryset
+    return Message.objects.filter(user__first_name=first_name).count()
 
 
 def get_top_users_by_number_of_the_messages() -> list[User]:
-    queryset = User.objects.annotate(
+    return User.objects.annotate(
         num_messages=Count("message")).order_by("-num_messages")[:3]
-    return queryset
 
 
 def get_last_5_messages_dicts() -> list[dict]:
