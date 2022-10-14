ad0a44477edfc81cbc44b825f851a020d61e2747
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-10-14 17:23:36 -0300

    Solution

--- db/queries.py
+++ db/queries.py
@@ -1,46 +1,96 @@
 from db.models import Message, User, Chat
 from django.db.models import Q, Count, F
+#import init_django_orm  # noqa: F401
 
 
 def get_messages_that_contain_word(word: str) -> list[Message]:
-    pass
+    queryset = Message.objects.filter(text__icontains=word)
+    return list(queryset)
 
 
 def get_untitled_chats() -> list[Chat]:
-    pass
+    queryset = Chat.objects.filter(title__startswith="Untitled")
+    return list(queryset)
 
 
 def get_users_who_sent_messages_in_2015() -> list[str]:
-    pass
+    queryset = Message.objects.filter(
+        sent__year=2015
+    ).values_list(
+        "user__first_name", "user__last_name"
+    )
+    return list(queryset)
 
 
 def get_actual_chats() -> list[Chat]:
-    pass
+    queryset = Chat.objects.filter(message__sent__year__gt=2020)
+    return list(queryset)
 
 
 def get_messages_contain_authors_first_name():
-    pass
+    queryset = Message.objects.filter(
+        text__icontains=F("user__first_name")
+    )
+    return queryset
 
 
 def get_users_who_sent_messages_starts_with_m_or_a() -> list[User]:
-    pass
+    queryset = User.objects.filter(
+        Q(message__text__istartswith="a") | Q(message__text__istartswith="m")
+    )
+    return list(queryset)
 
 
 def get_delivered_or_admin_messages() -> list[Message]:
-    pass
+    queryset = Message.objects.filter(
+        Q(is_delivered=True) | Q(user__username__istartswith="admin")
+    )
+    return list(queryset)
 
 
 def get_count_messages_sent_by_first_name(first_name: str) -> int:
-    pass
+    queryset = Message.objects.values("user__first_name").annotate(
+        count_messages=Count("text"))
+
+    for user in queryset:
+        for key, value in user.items():
+            if key == "user__first_name" and value == first_name:
+                return user["count_messages"]
 
 
 def get_top_users_by_number_of_the_messages() -> list[User]:
-    pass
+    queryset = User.objects.annotate(
+        num_messages=Count("message__text")
+    ).order_by("-num_messages")[:3]
+    return list(queryset)
 
 
 def get_last_5_messages_dicts() -> list[dict]:
-    pass
+    queryset = Message.objects.annotate(
+        username=F("user__username")
+    ).select_related("user").extra(
+        select={"from": "username"}
+    ).values("from", "text").order_by("-sent")[:5]
+
+    return list(queryset)
 
 
 def get_chat_dicts() -> list[dict]:
-    pass
+    queryset = Chat.objects.prefetch_related("users").annotate(
+        username=F("users__username")
+    ).order_by("id").extra(
+        select={"users": "username"}
+    ).values(
+        "id", "title", "users"
+    )
+
+    new_list = []
+    for chat in queryset:
+        id_num = chat["id"]
+        if new_list and id_num == new_list[-1]["id"]:
+            new_list[-1]["users"].append(chat["users"])
+        else:
+            new_list.append({"id": id_num,
+                             "title": chat["title"],
+                             "users": [chat["users"]]})
+    return new_list
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import init_django_orm  # noqa: F401
 
 from db.models import User, Chat, Message  # noqa: F401
+from db.queries import get_top_users_by_number_of_the_messages, get_messages_contain_authors_first_name
 
 
 def main():
-    pass
+    print(get_top_users_by_number_of_the_messages())
 
 
 if __name__ == "__main__":
-    print(main())
+    main()
