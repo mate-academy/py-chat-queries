cb9ace98fe6c3a65f73604458f638626c95f2bdf
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-10-15 13:26:31 -0300

    Solution update

--- db/queries.py
+++ db/queries.py
@@ -26,7 +26,7 @@ def get_actual_chats() -> list[Chat]:
     return list(queryset)
 
 
-def get_messages_contain_authors_first_name() -> list[Message]:
+def get_messages_contain_authors_first_name():
     queryset = Message.objects.filter(
         text__icontains=F("user__first_name")
     )
