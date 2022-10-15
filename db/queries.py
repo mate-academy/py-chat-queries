9e74fe595280f98fad62455c6546006363b2af41
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-10-15 13:35:14 -0300

    Solution update

--- db/queries.py
+++ db/queries.py
@@ -26,15 +26,11 @@ def get_actual_chats() -> list[Chat]:
     return list(queryset)
 
 
-def get_messages_contain_authors_first_name() -> list[Message]:
+def get_messages_contain_authors_first_name():
     queryset = Message.objects.filter(
         text__icontains=F("user__first_name")
     )
     return list(queryset)
-
-
-def get_messages_contain_authors_first_name() -> list[Message]:
-    pass
 
 
 def get_users_who_sent_messages_starts_with_m_or_a() -> list[User]:
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import init_django_orm  # noqa: F401
 from db.models import User, Chat, Message  # noqa: F401
-from db.queries import get_messages_contain_authors_first_name
 
 
 def main():
