0664ef84856c73d255c2d3e4a606592e076395d1
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-10-15 13:20:44 -0300

    Solution update

--- db/queries.py
+++ db/queries.py
@@ -31,8 +31,6 @@ def get_messages_contain_authors_first_name() -> list[Message]:
         text__icontains=F("user__first_name")
     )
     return list(queryset)
-    # return list(Message.objects.filter(
-    #     text__icontains=F("user__first_name")))
 
 
 def get_users_who_sent_messages_starts_with_m_or_a() -> list[User]:
