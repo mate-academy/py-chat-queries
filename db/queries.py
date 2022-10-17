815bc1d5b67f30b1cce46d3f6f14dc090e7ce3a0
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-10-17 18:40:48 +0300

    Reformatted file

--- db/queries.py
+++ db/queries.py
@@ -11,41 +11,39 @@ def get_untitled_chats() -> list[Chat]:
 
 
 def get_users_who_sent_messages_in_2015() -> list[str]:
-    return list(Message.objects.filter(
-        sent__year="2015"
-    ).values_list(
-        "user__first_name", "user__last_name"
-    ))
+    return list(
+        Message.objects.filter(sent__year="2015").values_list(
+            "user__first_name", "user__last_name"
+        )
+    )
 
 
 def get_actual_chats() -> list[Chat]:
-    return list(Chat.objects.filter(
-        message__sent__year__gte="2020"
-    ).distinct())
+    return list(Chat.objects.filter(message__sent__year__gte="2020").distinct())
 
 
 def get_messages_contain_authors_first_name() -> list[Message]:
-    return list(Message.objects.filter(
-        text__icontains=F("user__first_name")
-    ))
+    return list(Message.objects.filter(text__icontains=F("user__first_name")))
 
 
 def get_users_who_sent_messages_starts_with_m_or_a() -> list[User]:
-    return list(User.objects.filter(
-        Q(message__text__istartswith="a") | Q(message__text__istartswith="m")
-    ))
+    return list(
+        User.objects.filter(
+            Q(message__text__istartswith="a") | Q(message__text__istartswith="m")
+        )
+    )
 
 
 def get_delivered_or_admin_messages() -> list[Message]:
-    return list(Message.objects.filter(
-        Q(is_delivered=True) | Q(user__username__startswith="admin")
-    ))
+    return list(
+        Message.objects.filter(
+            Q(is_delivered=True) | Q(user__username__startswith="admin")
+        )
+    )
 
 
 def get_count_messages_sent_by_first_name(first_name: str) -> int:
-    return Message.objects.filter(
-        user__first_name=first_name
-    ).count()
+    return Message.objects.filter(user__first_name=first_name).count()
 
 
 def get_top_users_by_number_of_the_messages() -> list[User]:
@@ -56,12 +54,8 @@ def get_top_users_by_number_of_the_messages() -> list[User]:
 
 def get_last_5_messages_dicts() -> list[dict]:
     return list(
-        {
-            "from": message.user.username,
-            "text": message.text
-        }
-        for message in
-        Message.objects.order_by("-sent").select_related("user")[:5]
+        {"from": message.user.username, "text": message.text}
+        for message in Message.objects.order_by("-sent").select_related("user")[:5]
     )
 
 
@@ -70,7 +64,7 @@ def get_chat_dicts() -> list[dict]:
         {
             "id": chat.id,
             "title": chat.title,
-            "users": [user.username for user in chat.users.all()]
+            "users": [user.username for user in chat.users.all()],
         }
         for chat in Chat.objects.prefetch_related("users")
     )
