e57c8455842ee2f7f144030168acdc43afbecdff
Vasyl Poremchuk <nleveryone@gmail.com>
2022-10-23 22:48:34 +0300

    The method all() has been deleted from the function get_chat_dicts()

--- db/queries.py
+++ db/queries.py
@@ -65,5 +65,5 @@ def get_chat_dicts() -> list[dict]:
             "id": chat.id, "title": chat.title,
             "users": [user.username for user in chat.users.all()],
         }
-        for chat in Chat.objects.all().prefetch_related("users")
+        for chat in Chat.objects.prefetch_related("users")
     ]
