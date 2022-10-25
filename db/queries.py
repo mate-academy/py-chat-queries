6962f7cf552d128b768348712f9fc26bf4db4aa7
igor kosianenko <funkcionalkos@gmail.com>
2022-10-25 11:32:54 +0300

    del blank line

--- db/queries.py
+++ db/queries.py
@@ -10,14 +10,12 @@ def get_messages_that_contain_word(word: str) -> list[Message]:
 
 
 def get_untitled_chats() -> list[Chat]:
-
     return list(
         Chat.objects.filter(title__startswith="Untitled")
     )
 
 
 def get_users_who_sent_messages_in_2015() -> list[str]:
-
     return list(
         Message.objects.filter(
             sent__year=2015
@@ -29,19 +27,16 @@ def get_users_who_sent_messages_in_2015() -> list[str]:
 
 
 def get_actual_chats() -> list[Chat]:
-
     return list(Chat.objects.filter(message__sent__year__gt=2020))
 
 
 def get_messages_contain_authors_first_name() -> list[Message]:
-
     return list(
         Message.objects.filter(text__icontains=F("user__first_name"))
     )
 
 
 def get_users_who_sent_messages_starts_with_m_or_a() -> list[User]:
-
     return list(
         User.objects.filter(
             Q(message__text__istartswith="m")
@@ -51,7 +46,6 @@ def get_users_who_sent_messages_starts_with_m_or_a() -> list[User]:
 
 
 def get_delivered_or_admin_messages() -> list[Message]:
-
     return list(
         Message.objects.filter(
             Q(user__username__istartswith="admin")
@@ -61,12 +55,10 @@ def get_delivered_or_admin_messages() -> list[Message]:
 
 
 def get_count_messages_sent_by_first_name(first_name: str) -> int:
-
     return Message.objects.filter(user__first_name=first_name).count()
 
 
 def get_top_users_by_number_of_the_messages() -> list[User]:
-
     return list(
         User.objects.annotate(
             num_messages=Count("message")
@@ -75,7 +67,6 @@ def get_top_users_by_number_of_the_messages() -> list[User]:
 
 
 def get_last_5_messages_dicts() -> list[dict]:
-
     return list(
         Message.objects.order_by(
             "-sent"
@@ -86,7 +77,6 @@ def get_last_5_messages_dicts() -> list[dict]:
 
 
 def get_chat_dicts() -> list[dict]:
-
     return [
         {
             "id": chat.id,
