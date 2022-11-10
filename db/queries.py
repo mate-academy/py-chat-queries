4fbc77dcd1615f866dbd9c55f9ec84f869fbf01d
Diana <daria.blessed18@gmail.com>
2022-11-10 15:33:50 +0200

    fixed

--- db/queries.py
+++ db/queries.py
@@ -7,34 +7,34 @@ def get_messages_that_contain_word(word: str) -> list[Message]:
 
 
 def get_untitled_chats() -> list[Chat]:
-    return list(Chat.objects.filter(
-        title__startswith="Untitled"))
+    return list(Chat.objects.filter(title__startswith="Untitled"))
 
 
 def get_users_who_sent_messages_in_2015() -> list[str]:
-    return list(Message.objects.filter(
-        sent__year=2015).values_list("user__first_name",
-                                     "user__last_name"))
+    return list(
+        Message.objects.filter(sent__year=2015).values_list(
+            "user__first_name", "user__last_name")
+    )
 
 
 def get_actual_chats() -> list[Chat]:
-    return list(Chat.objects.filter(
-        message__sent__year__gt=2020))
+    return list(Chat.objects.filter(message__sent__year__gt=2020))
 
 
 def get_messages_contain_authors_first_name() -> list[Message]:
-    return list(
-        Message.objects.filter(text__contains=F("user__first_name")))
+    return list(Message.objects.filter(text__contains=F("user__first_name")))
 
 
 def get_users_who_sent_messages_starts_with_m_or_a() -> list[User]:
     return list(User.objects.filter(
-        Q(message__text__istartswith="a") | Q(message__text__istartswith="m")))
+        Q(message__text__istartswith="a") | Q(message__text__istartswith="m"))
+    )
 
 
 def get_delivered_or_admin_messages() -> list[Message]:
     return list(Message.objects.filter(
-        Q(user__first_name__startswith="admin") | Q(is_delivered=True)))
+        Q(user__first_name__startswith="admin") | Q(is_delivered=True))
+    )
 
 
 def get_count_messages_sent_by_first_name(first_name: str) -> int:
@@ -42,8 +42,10 @@ def get_count_messages_sent_by_first_name(first_name: str) -> int:
 
 
 def get_top_users_by_number_of_the_messages() -> list[User]:
-    return list(User.objects.annotate(
-        num_messages=Count("message")).order_by("-num_messages")[:3])
+    return list(
+        User.objects.annotate(
+            num_messages=Count("message")).order_by("-num_messages")[:3]
+    )
 
 
 def get_last_5_messages_dicts() -> list[dict]:
