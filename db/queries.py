f5aee9fab0d7798115c9099866f2d989ee63cfde
Yulia Bozhieva <bozhieva93@gmail.com>
2022-11-24 13:05:23 -0400

    'Solution'

--- db/queries.py
+++ db/queries.py
@@ -46,9 +46,9 @@ def get_delivered_or_admin_messages() -> list[Message]:
 
 
 def get_count_messages_sent_by_first_name(first_name: str) -> int:
-    return Message.objects.\
-        filter(user__first_name=first_name).\
-        select_related("user").count()
+    return Message.objects.filter(
+        user__first_name=first_name
+    ).select_related("user").count()
 
 
 def get_top_users_by_number_of_the_messages() -> list[User]:
