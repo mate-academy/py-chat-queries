49326e108107fdde2645ce3f451ba726ffc2b10e
Taras Martyniuk <bythewaters800@gmail.com>
2022-12-19 19:42:27 +0100

    Solution2

--- db/queries.py
+++ db/queries.py
@@ -15,11 +15,11 @@ def get_untitled_chats() -> list[Chat]:
 
 
 def get_users_who_sent_messages_in_2015() -> list[str]:
-    message = Message.objects.filter(
-        sent__year="2015"
-    ).values_list("user__first_name", "user__last_name")
+    users = User.objects.filter(
+        message__sent__year="2015"
+    ).values_list("first_name", "last_name")
 
-    return [info for info in message]
+    return [user for user in users]
 
 
 def get_actual_chats() -> list[Chat]:
@@ -29,14 +29,14 @@ def get_actual_chats() -> list[Chat]:
 
 
 def get_messages_contain_authors_first_name() -> list[Message]:
-    messages = Message.objects.filter(text__icontains=F("user__first_name"))
-
-    return [message for message in messages]
+    return list(
+        Message.objects.filter(text__icontains=F("user__first_name"))
+    )
 
 
 def get_users_who_sent_messages_starts_with_m_or_a() -> list[User]:
     message = User.objects.filter(
-        Q(message__text__startswith="a") | Q(message__text__startswith="m")
+        Q(message__text__istartswith="a") | Q(message__text__istartswith="m")
     )
 
     return [user for user in message.all()]
@@ -51,11 +51,9 @@ def get_delivered_or_admin_messages() -> list[Message]:
 
 
 def get_count_messages_sent_by_first_name(first_name: str) -> int:
-    message = Message.objects.filter(
+    return Message.objects.filter(
         user__first_name=first_name
-    ).aggregate(Count("user"))
-
-    return message["user__count"]
+    ).count()
 
 
 def get_top_users_by_number_of_the_messages() -> list[User]:
