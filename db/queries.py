6eb4b540dc42fbde0f281de4007b51dd0d089e79
Vika <viktorykoroliova@gmail.com>
2022-12-19 17:54:56 +0200

    solution

--- db/queries.py
+++ db/queries.py
@@ -54,8 +54,8 @@ def get_last_5_messages_dicts() -> list[dict]:
             "from": message.user.username,
             "text": message.text
         }
-        for message in Message.objects.select_related(
-            "user").order_by("-sent")[:5]
+        for message in Message.objects.select_related("user")
+        .order_by("-sent")[:5]
     )
 
 
