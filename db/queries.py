2dfdb1e2ab1291806025419be05f0086862dd7da
ViktorKr <kravchukviktors@gmail.com>
2023-01-23 19:38:26 +0200

    Solution

--- db/queries.py
+++ db/queries.py
@@ -24,20 +24,17 @@ def get_actual_chats() -> list[Chat]:
 
 def get_messages_contain_authors_first_name() -> list[Message]:
     return list(Message.objects.filter(
-        text__contains=F("user__first_name"))
-    )
+        text__contains=F("user__first_name")))
 
 
 def get_users_who_sent_messages_starts_with_m_or_a() -> list[User]:
     return list(User.objects.filter(
-        Q(message__text__startswith="m") | Q(message__text__startswith="a"))
-    )
+        Q(message__text__startswith="m") | Q(message__text__startswith="a")))
 
 
 def get_delivered_or_admin_messages() -> list[Message]:
     return list(Message.objects.filter(
-        Q(is_delivered=True) | Q(user__username__startswith="admin"))
-    )
+        Q(is_delivered=True) | Q(user__username__startswith="admin")))
 
 
 def get_count_messages_sent_by_first_name(first_name: str) -> int:
@@ -46,8 +43,7 @@ def get_count_messages_sent_by_first_name(first_name: str) -> int:
 
 def get_top_users_by_number_of_the_messages() -> list[User]:
     return list(User.objects.annotate(
-        num_messages=Count("message")
-    ).order_by("-num_messages"))[:3]
+        num_messages=Count("message")).order_by("-num_messages"))[:3]
 
 
 def get_last_5_messages_dicts() -> list[dict]:
@@ -58,7 +54,7 @@ def get_last_5_messages_dicts() -> list[dict]:
 
 
 def get_chat_dicts() -> list[dict]:
-    queryset = Chat.objects.all().prefetch_related("users")
+    queryset = Chat.objects.prefetch_related("users")
     return [{
         "id": chat.pk, "title": chat.title,
         "users": [user.username for user in chat.users.all()]
