f7c35a8690212e06b078e32eee7095e557405df5
ViktorKr <kravchukviktors@gmail.com>
2023-01-24 10:51:40 +0200

    Solution

--- db/queries.py
+++ db/queries.py
@@ -23,18 +23,27 @@ def get_actual_chats() -> list[Chat]:
 
 
 def get_messages_contain_authors_first_name() -> list[Message]:
-    return list(Message.objects.filter(
-        text__contains=F("user__first_name")))
+    return list(
+        Message.objects.filter(
+            text__contains=F("user__first_name")
+        )
+    )
 
 
 def get_users_who_sent_messages_starts_with_m_or_a() -> list[User]:
-    return list(User.objects.filter(
-        Q(message__text__startswith="m") | Q(message__text__startswith="a")))
+    return list(
+        User.objects.filter(
+            Q(message__text__startswith="m") | Q(message__text__startswith="a")
+        )
+    )
 
 
 def get_delivered_or_admin_messages() -> list[Message]:
-    return list(Message.objects.filter(
-        Q(is_delivered=True) | Q(user__username__startswith="admin")))
+    return list(
+        Message.objects.filter(
+            Q(is_delivered=True) | Q(user__username__startswith="admin")
+        )
+    )
 
 
 def get_count_messages_sent_by_first_name(first_name: str) -> int:
@@ -47,10 +56,16 @@ def get_top_users_by_number_of_the_messages() -> list[User]:
 
 
 def get_last_5_messages_dicts() -> list[dict]:
-    queryset = Message.objects.select_related(
-        "user").order_by("-sent")[:5]
-    return [{"from": message.user.username, "text": message.text}
-            for message in queryset]
+    queryset = (
+        Message.objects
+        .select_related("user")
+        .order_by("-sent")
+    )[:5]
+
+    return [{
+        "from": message.user.username,
+        "text": message.text
+    } for message in queryset]
 
 
 def get_chat_dicts() -> list[dict]:
